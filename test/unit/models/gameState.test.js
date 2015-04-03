9cc733ed19dd93e9aa30a9f3a86d9687a8ffa2b6
Darren M <aeneascorrupt@gmail.com>
2015-04-02 23:49:04 -0700

    Enum for game states

---
+++ test/unit/models/gameState.test.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+
+describe.only('Game State', function describeGameState() {
+  var gameStates = require('../../../lib/models/gameState');
+
+  it('has the CREATED state', function testCreated() {
+    var next = [];
+    var properties;
+
+    expect(gameStates).to.have.property('CREATED');
+    expect(gameStates).to.have.property('properties')
+      .that.has.property(gameStates.CREATED);
+
+    properties = gameStates.properties[gameStates.CREATED];
+    expect(properties).to.have.property('label')
+      .that.deep.equal('created');
+    expect(properties).to.have.property('next')
+      .that.deep.equal(next);
+  });
+});
