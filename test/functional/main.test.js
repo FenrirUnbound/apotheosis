8267a71af17905a5e9c9190c3ce64e0da1506d42
Darren M <aeneascorrupt@gmail.com>
2015-04-04 18:15:43 -0700

    generate random player during game creation & set cookie

--- test/functional/main.test.js
+++ test/functional/main.test.js
@@ -1,3 +1,5 @@
+var atob = require('atob');
+var cookie = require('cookie');
 var expect = require('chai').expect;
 var mockery = require('mockery');
 var path = require('path');
@@ -66,6 +68,17 @@ describe('Main', function describeMain() {
   });
 
   describe('-- Games', function describeGames() {
+    function checkAndGetCookies(response) {
+      var cookies;
+
+      expect(response.headers).to.have.property('set-cookie');
+
+      cookies = cookie.parse(response.headers['set-cookie'].shift());
+      expect(cookies).to.have.property('player');
+
+      return JSON.parse(atob(cookies.player));
+    }
+
     it('should create a game', function testCreateGame(done) {
       var CREATED_STATE = 0;
 
@@ -74,15 +87,17 @@ describe('Main', function describeMain() {
         url: '/api/games'
       }, function (response) {
         var data;
+        var playerData;
         expect(response.statusCode).to.equal(201);
+        playerData = checkAndGetCookies(response);
+
         data = JSON.parse(response.payload);
         expect(data).to.have.property('gameId')
           .that.is.greaterThan(0);
         expect(data).to.have.property('gameState')
           .that.equal(CREATED_STATE);
         expect(data).to.have.property('creator')
-          .that.is.greaterThan(0)
-          .and.is.at.most(Date.now());
+          .that.equal(playerData.playerId);
         done();
       });
     });
