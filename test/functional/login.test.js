1e377f5793d12f89ff85ba2250014e3d2c098818
Darren M <aeneascorrupt@gmail.com>
2015-04-05 15:37:23 -0700

    validate cookie is passed in from server

--- test/functional/login.test.js
+++ test/functional/login.test.js
@@ -1,8 +1,10 @@
+var btoa = require('btoa');
+var cookie = require('cookie');
 var expect = require('chai').expect;
 var mockery = require('mockery');
 var path = require('path');
 
-describe('Login', function describeLogin() {
+describe.only('Login', function describeLogin() {
   var server;
 
   before(function onceBefore() {
@@ -37,12 +39,35 @@ describe('Login', function describeLogin() {
     mockery.disable();
   });
 
-  it('should validate a pseudo login', function testLogin(done) {
+  function parseForCookies(response) {
+    var headerInformation = response.headers['set-cookie'].pop();
+    var cookies = cookie.parse(headerInformation);
+    var result = {};
+
+    Object.keys(cookies).forEach(function (key) {
+      var buffer = new Buffer(cookies[key], 'base64');
+      result[key] = JSON.parse(buffer);
+    });
+
+    return result;
+  }
+
+  it('should validate a login', function testLogin(done) {
+    var testId = 88888888;
+
     server.inject({
       method: 'POST',
-      url: '/api/login'
+      url: '/api/login',
+      payload: {playerId: testId}
     }, function verify(response) {
+      var cookies;
       expect(response.statusCode).to.equal(204);
+      expect(response.headers).to.have.property('set-cookie');
+
+      cookies = parseForCookies(response);
+      expect(cookies).to.have.property('player')
+        .that.equal(testId);
+
       done();
     });
   });
